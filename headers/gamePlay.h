1717c6d4a46ff6e8faf7f184c153e4a6c9a2f12b
ronak-suthar <ronakdj786@gmail.com>
2021-09-19 09:30:16 +0530

    match parameters updation

--- headers/gamePlay.h
+++ headers/gamePlay.h
@@ -1,13 +1,43 @@
 #ifndef GAMEPLAY_H
 #define GAMEPLAY_H
 #include "database.h"
+#include <queue>
+#include <utility>
+#include <vector>
 
 class gamePlay:public database{
     int gamePlayBalls,gamePlayOvers,gamePlayBatsmanId,gamePlayBowlerId,gamePlayRuns,gamePlayMatchId;
+    int gamePlayOut,gamePlayOverBound,gamePlayOutBound;
+    char isBatting;
+    char isBowling;
 
+    std::queue<std::pair<std::string, int>> boTeamA, boTeamB; //Batting Order Team Order
+
+    std::pair<std::string, std::string> teams; //Team A and Team B Name
+
+    //std::queue<std::pair<std::string, int>>& toBat,toBowl;
+    //above have refrence to teams batting order which is either batting or bowling
+
+    std::vector<std::pair<std::string, int>> batters;
+    //array of two batters who are batting
+
+    std::pair<std::string, int> bowler;
     public:
 
+    gamePlay(void);
     void startMatch(void);
+    void setBatters(int=1);
+    void setBowler(void);
+    void viewTeamOrder(std::queue<std::pair<std::string, int>>);
+
+    void setBounds();
+    void gameDisplay();
+    void getGameStatus();
+
+    std::queue<std::pair<std::string, int>> battingTeam();
+    std::queue<std::pair<std::string, int>> bowlingTeam();
+    void swapOrder(void);
 };
+
 
 #endif
--- implementation/gamePlay.cpp
+++ implementation/gamePlay.cpp
@@ -1,60 +1,325 @@
-#include"../headers/gamePlay.h"
-#include"../headers/menu.h"
-#include"../headers/Matches.h"
-#include"../headers/player.h"
-#include"../headers/Team.h"
-#include<iostream>
-#include<queue>
-#include<utility>
+#include "../headers/gamePlay.h"
+#include "../headers/menu.h"
+#include "../headers/Matches.h"
+#include "../headers/player.h"
+#include "../headers/Team.h"
+#include <iostream>
 
-void gamePlay::startMatch(){
-    Matches matchInfo;Player playerInfo;Team teamInfo;
+gamePlay::gamePlay(void)
+{
+    batters.resize(2);
+    gamePlayBalls = 0;
+    gamePlayRuns = 0;
+    gamePlayOut = 0;
+    gamePlayOvers = 0;
+    // toBat=boTeamA;
+    // toBowl=boTeamB;
+    isBatting = 'A';
+    isBowling = 'B';
+    setBounds();
+}
+void gamePlay::gameDisplay(void)
+{
+    system("clear");
+    print("---------------------------------", 2);
+    print("Welcome To Cricket Match Console", 2);
+    print("---------------------------------", 2);
 
-    print("Welcome To Live Match Managment Section",2);
-    print("Enter ID of Match : ",0);
-    std::cin>>gamePlayMatchId;
+    print("Match Teams : " + teams.first + " vs " + teams.second, 2);
+
+    print("Runs Scored/Out : " + std::to_string(gamePlayRuns) + "/" + std::to_string(gamePlayOut));
+    print(" Overs : " + std::to_string(gamePlayOvers) + "." + std::to_string(gamePlayBalls), 2);
+
+    print("Striker : " + batters[0].first, 2);
+    print("Non Striker : " + batters[1].first, 2);
+    print("Bowler : " + bowler.first, 4);
+
+    getGameStatus();
+}
+
+void gamePlay::setBounds(void)
+{
+    gamePlayOutBound = 10;
+    std::string query = "SELECT MatchType From Matches Where MatchID=" + std::to_string(gamePlayMatchId);
+    std::string result;
+    try
+    {
+        if (!execute_query(query))
+        {
+            throw 0;
+        }
+
+        res = mysql_use_result(conn);
+
+        row = mysql_fetch_row(res);
+
+        if (row != NULL)
+        {
+            std::string result = row[0];
+        }
+
+        if (result == "T20")
+        {
+            gamePlayOverBound = 20;
+        }
+        else if (result == "ODI")
+        {
+            gamePlayOverBound = 50;
+        }
+        else
+        {
+            //Handle Custom Scene of 90 Overs Per Day Later
+            gamePlayOverBound = 90;
+        }
+
+        mysql_free_result(res);
+    }
+    catch (int e)
+    {
+        print("This Operation has issues", 1);
+    }
+}
+void gamePlay::getGameStatus(void)
+{
+    int choice;
+
+    print("***********************************************", 2);
+    print("Select Runs Scored at Overs " + std::to_string(gamePlayOvers) + "." + std::to_string(gamePlayBalls), 2);
+    print("1. Sixer", 2);
+    print("2. Four-Boundary", 2);
+    print("3. Out", 2);
+    print("4. Enter The Runs", 2);
+    print("Any other value to stop this operation", 3);
+
+    print("Enter Your Choice: ", 0);
+    std::cin >> choice;
     getchar();
 
-    if(!matchInfo.matchExists(gamePlayMatchId)){
+    std::string query;
+
+    if (choice == 1)
+    {
+        gamePlayRuns += 6;
+        query = "INSERT INTO ScoreCard VALUES (" + std::to_string(gamePlayBalls) + "," + std::to_string(gamePlayOvers) + "," + std::to_string(6) + "," + std::to_string(gamePlayMatchId) + ",\"" + batters[0].first + "\",\"" + bowler.first + "\")";
+    }
+    else if (choice == 2)
+    {
+        gamePlayRuns += 4;
+        query = "INSERT INTO ScoreCard VALUES (" + std::to_string(gamePlayBalls) + "," + std::to_string(gamePlayOvers) + "," + std::to_string(4) + "," + std::to_string(gamePlayMatchId) + ",\"" + batters[0].first + "\",\"" + bowler.first + "\")";
+    }
+    else if (choice == 3)
+    {
+        //outprocess
+        query = "INSERT INTO ScoreCard VALUES (" + std::to_string(gamePlayBalls) + "," + std::to_string(gamePlayOvers) + "," + std::to_string(404) + "," + std::to_string(gamePlayMatchId) + ",\"" + batters[0].first + "\",\"" + bowler.first + "\")";
+        gamePlayOut += 1;
+
+        if (gamePlayOutBound == gamePlayOut)
+        {
+            //constraints
+            swap(teams.first, teams.second);
+            swapOrder();
+        }
+        setBatters();
+    }
+    else if (choice == 4)
+    {
+        int runsScored;
+        print("Enter The Runs Scored : ", 0);
+        std::cin >> runsScored;
+        getchar();
+
+        query = "INSERT INTO ScoreCard VALUES (" + std::to_string(gamePlayBalls) + "," + std::to_string(gamePlayOvers) + "," + std::to_string(runsScored) + "," + std::to_string(gamePlayMatchId) + ",\"" + batters[0].first + "\",\"" + bowler.first + "\")";
+
+        gamePlayRuns += runsScored;
+
+        if (runsScored % 2 != 0)
+        {
+            swap(batters[0], batters[1]);
+        }
+    }
+    else
+    {
         return;
     }
 
-    std::queue<std::pair<std::string,int>> boTeamA,boTeamB;//Batting Order Team Order
+    try
+    {
+        std::cout << query << "\n";
+        getchar();
+        if (!execute_query(query))
+        {
+            throw 0;
+        }
+    }
+    catch (int e)
+    {
+        print("This Operation has issues", 1);
+    }
 
-    std::pair<std::string,std::string> teams;//Team A and Team B Name
+    if (gamePlayBalls == 6)
+    {
+        setBowler();
+        swap(batters[0], batters[1]);
+        gamePlayOvers+=1;
+        gamePlayBalls = 1;
+    }
+    else
+    {
+        gamePlayBalls += 1;
+    }
+    gameDisplay();
+}
+void gamePlay::setBatters(int toBeSet)
+{   
 
-    matchInfo.setTeams(teams,gamePlayMatchId);
+    if (isBatting == 'A')
+    {
+        for (int i = 0; i < toBeSet; i++)
+        {
+            batters[i].first = boTeamA.front().first;
+            batters[i].second = boTeamA.front().second;
+            boTeamA.pop();
+        }
+    }
+    else
+    {
+        for (int i = 0; i < toBeSet; i++)
+        {
+            batters[i].first = boTeamB.front().first;
+            batters[i].second = boTeamB.front().second;
+            boTeamB.pop();
+        }
+    }
 
-    print("Set Batting Order Of TeamA : ",2);
+    return;
+}
+
+void gamePlay::setBowler(void)
+{
+    print("Choose Player ID from Below Who Will be Bowling", 2);
+    viewTeamOrder(bowlingTeam());
+
+    print("Enter Player Name : ", 0);
+    std::getline(std::cin, bowler.first);
+
+    print("Enter Bowler ID : ", 0);
+    std::cin >> bowler.second;
+}
+
+void gamePlay::viewTeamOrder(std::queue<std::pair<std::string, int>> team)
+{
+    // std::cout << "Test Running\n";
+    // std::cout << team.size() << "\n";
+    // std::cout << boTeamA.size() << "\n";
+    // std::cout << boTeamB.size() << "\n";
+
+    for (int i = 1; team.empty() != true; i++)
+    {
+        print(std::to_string(i) + " : " + team.front().first + std::string(", ") + std::to_string(team.front().second) + std::string("\n"));
+        //std::cout<<team.front().first<<", "<<team.front().second<<"\n";
+        team.pop();
+    }
+
+    // std::cout << "Test Running After\n";
+    // std::cout << team.size() << "\n";
+    // std::cout << boTeamA.size() << "\n";
+    // std::cout << boTeamB.size() << "\n";
+
+    getchar();
+}
+void gamePlay::startMatch()
+{
+    Matches matchInfo;
+    Player playerInfo;
+    Team teamInfo;
+
+    print("Welcome To Live Match Managment Section", 2);
+    print("Enter ID of Match : ", 0);
+    std::cin >> gamePlayMatchId;
+    getchar();
+
+    if (!matchInfo.matchExists(gamePlayMatchId)) //Check if Match ID Exists in Record
+    {
+        return;
+    }
+
+    matchInfo.setTeams(teams, gamePlayMatchId); //set teams pair declared above
+
+    // BATTING ORDER OF TEAM'S Setup Begins
+    print("Set Batting Order Of TeamA : ", 2);
 
     playerInfo.displayAllPlayers(teams.first);
 
     playerInfo.setupBattingOrder(boTeamA);
 
     //Create View Team Function in Teams;
-    for(int i=1;boTeamA.empty()!=true;i++){
-        print(std::to_string(i)+" : "+boTeamA.front().first + std::string(", ") + std::to_string(boTeamA.front().second) + std::string("\n"));
-        //std::cout<<boTeamA.front().first<<", "<<boTeamA.front().second<<"\n";
-        boTeamA.pop();
-    }
+    viewTeamOrder(boTeamA);
 
-    print("Press Any Key to Continue Setting Up TeamB Order",2);
+    print("Press Any Key to Continue Setting Up TeamB Order", 2);
     getchar();
     system("clear");
 
-    print("Set Batting Order Of TeamB : ",2);
+    print("Set Batting Order Of TeamB : ", 2);
 
     playerInfo.displayAllPlayers(teams.second);
 
     playerInfo.setupBattingOrder(boTeamB);
 
     //Create View Team Function in Teams;
-    for(int i=1;boTeamB.empty()!=true;i++){
-        print(std::to_string(i)+" : "+boTeamB.front().first + std::string(", ") + std::to_string(boTeamB.front().second) + std::string("\n"));
-        //std::cout<<boTeamB.front().first<<", "<<boTeamB.front().second<<"\n";
-        boTeamB.pop();
+    viewTeamOrder(boTeamB);
+
+    print("Press Any Key to Continue With Toss Result", 2);
+    getchar();
+    system("clear");
+
+    // BATTING ORDER OF TEAM'S Setup Ends
+
+    //Batting Order Of Team whichs bats first Based on Toss
+
+    int choice;
+
+    print("Select the Team That will Bat First : ");
+
+    print("1. " + teams.first, 1);
+    print("2. " + teams.second, 1);
+
+    print("Select Your Choice : ", 0);
+    std::cin >> choice;
+    getchar();
+
+    if (choice == 2)
+    {
+        swap(teams.first, teams.second);
+        swapOrder();
     }
 
-    
+    //Inital Setup When Game Begins
+    setBatters(2);
+    setBowler();
 
+    gameDisplay();
+
+    gamePlayOvers = 1;
+    gamePlayBalls = 1;
+}
+
+std::queue<std::pair<std::string, int>> gamePlay::battingTeam(void)
+{
+    if (isBatting == 'A')
+    {
+        return boTeamA;
+    }
+    return boTeamB;
+}
+std::queue<std::pair<std::string, int>> gamePlay::bowlingTeam(void)
+{
+    if (isBatting == 'A')
+    {
+        return boTeamB;
+    }
+    return boTeamA;
+}
+void gamePlay::swapOrder(void)
+{   
+    std::swap(isBatting, isBowling);
 }
